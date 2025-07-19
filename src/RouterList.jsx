943ab27c7b7395b1c103f5ec7959446b0c587626
shailup50 <shailendra@cruxcreativesolutions.com>
2025-07-19 19:20:55 +0530

    fix route

--- src/RouterList.jsx
+++ src/RouterList.jsx
@@ -22,11 +22,11 @@ function RouterList() {
             <BrowserRouter>
                 <Header />
                 <Routes>
-                    <Route path="/" element={<Home />} />
+                    <Route  path="/" element={<Home />} />
                     <Route path="/support" element={<Support />} />
-                    <Route path='/products/axial-fans' element={<AxialFans />} />
-                    <Route path='/products/mobility-fans' element={<MobilityFans />} />
-                    <Route path='/products/forward-curves-centrifugal-fan/double-inlet-centrifugal-fans' element={<DoubleInletCentrifugalFans />} />
+                    <Route exact path='/products/axial-fans' element={<AxialFans />} />
+                    <Route exact path='/products/mobility-fans' element={<MobilityFans />} />
+                    <Route exact path='/products/forward-curves-centrifugal-fan/double-inlet-centrifugal-fans' element={<DoubleInletCentrifugalFans />} />
                 </Routes>
                 <Footer />
             </BrowserRouter>
